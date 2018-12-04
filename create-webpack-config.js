84140ef6f966fd45ce7bfb1e3e96848b1dc553ea
Marek <marekz@gmail.com>
2018-12-04 17:35:30 -0500

    Add extension to entry

--- create-webpack-config.js
+++ create-webpack-config.js
@@ -40,7 +40,7 @@ module.exports = function createWebpackConfig(options = {}) {
     // Add HMR client file to each entry
     each(entries, entry => {
       entry.push(
-        path.join(__dirname, '/node_modules/webpack-hot-middleware/client')
+        path.join(__dirname, '/node_modules/webpack-hot-middleware/client.js')
       );
     });
   }
