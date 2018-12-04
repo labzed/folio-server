f3167a605a34508e3717a02a4618575d9c4ea22a
Marek <marekz@gmail.com>
2018-12-04 16:59:46 -0500

    Add puppeteer and pdf printer

---
+++ printer.js
@@ -0,0 +1,46 @@
+const puppeteer = require('puppeteer');
+
+class Printer {
+  constructor() {
+    // eager mode, launch browser right away.
+    this.getBrowser();
+  }
+
+  getBrowser() {
+    if (!this.browserPromise) {
+      this.browserPromise = puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      });
+    }
+
+    return this.browserPromise;
+  }
+
+  async getNewPage() {
+    const browser = await this.getBrowser();
+    return browser.newPage();
+  }
+
+  async print(url) {
+    const page = await this.getNewPage();
+    try {
+      // const url = 'http://localhost:4000'; // TODO configure port
+      await page.goto(url, { waitUntil: 'networkidle2' });
+      const pdfOptions = {};
+      const pdf = await page.pdf(pdfOptions);
+      return pdf;
+    } catch (error) {
+      throw error;
+    } finally {
+      page.close();
+    }
+  }
+
+  async close() {
+    if (this.browserPromise) {
+      const browser = await this.browserPromise;
+      this.browserPromise = null;
+      browser.close();
+    }
+  }
+}
--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const path = require('path');
 const port = 4000;
+const Printer = require('./printer');
 
 module.exports = function server({ mode = 'development', rootDirectory } = {}) {
   rootDirectory = rootDirectory || process.cwd();
   const isProduction = mode === 'production';
 
   const app = express();
+
+  const router = expressPromiseRouter();
 
   const webpackCompiler = createWebpackFromFolioConfig({ mode });
 
@@ -21,6 +24,18 @@ module.exports = function server({ mode = 'development', rootDirectory } = {}) {
     app.use(webpackHotMiddleware(webpackCompiler, { reload: true }));
     app.use('/templates', webpackDevMiddleware(webpackCompiler));
   }
+
+  const printer = new Printer();
+
+  router.get('/render/:templateName', async (req, res, next) => {
+    const printer = await startPrinterPromise;
+    const payload = encodeURIComponent(req.query.payload);
+    const pdf = await printer.print(
+      `http://localhost:4000/templates/${templateName}#${payload}`
+    );
+  });
+
+  app.use(router);
 
   app.listen(port, () => {
     console.log(`[folio-server] listening on port ${port}`);
